dc918318839c4ebe9ffcbdaec2603912c084e81b
JGHolscher <holschjg@mail.uc.edu>
2022-09-01 13:24:29 -0400

    might die

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,82 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
 public class Person {
+    public static void main(String[] args) {
+        JFileChooser chooser = new JFileChooser();
+        String ID = "";
+        String fName = "";
+        String lName = "";
+        String title = "";
+
+        String fileName = "";
+        boolean more = true;
+        Scanner fileNamein = new Scanner(System.in);
+
+        ArrayList<String> People = new ArrayList<>();
+
+        // User enter Persons information
+        do {
+            Scanner in = new Scanner(System.in);
+            String person = "";
+
+
+            //System.out.printf("%10s", person);
+
+            ID = SafeInput.getNonZeroLenString(in, "Please enter ID Number") + ", ";
+            person += ID;
+
+            fName = SafeInput.getNonZeroLenString(in, "Please enter First Name") + ", ";
+            person += fName;
+
+            lName = SafeInput.getNonZeroLenString(in, "Please enter Last Name") + ", ";
+            person += lName;
+
+
+            title = SafeInput.getNonZeroLenString(in, "Please enter persons Title (Mr., Mrs., Ms., Dr., etc.)") + ", ";
+            person += title;
+
+            int YOB = SafeInput.getInt(in, "Please enter persons Birth Year");
+            person += YOB;
+
+            People.add(person);
+
+            System.out.println("\n");
+
+            more = SafeInput.getYNConfirm(in, "Would you like to enter another Person");
+        }while(more);
+
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        fileName = SafeInput.getNonZeroLenString(fileNamein, "Please enter file name (Don't include .txt)");
+        Path file = Paths.get(workingDirectory.getPath() + "//src//"+fileName+".txt");
+
+        try
+        {
+
+            OutputStream out =
+                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer =
+                    new BufferedWriter(new OutputStreamWriter(out));
+
+
+            for(String person : People )
+            {
+                writer.write(person, 0, person.length());
+                writer.newLine();
+            }
+            writer.close();
+            System.out.println("Data file written:" + fileName + ".txt");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -7,12 +7,20 @@ import java.util.*;
 import static java.nio.file.StandardOpenOption.CREATE;
 
 public class PersonGenerator {
-    Scanner in = new Scanner(System.in);
-    Calendar c = Calendar.getInstance();
-    private String ID;
-    private String fName;
 
-    public Person(String ID, String fName, String lName, String title, int YOB) {
+    //Calendar c = Calendar.getInstance();
+    String ID;
+    String fName;
+    String lName;
+    String title;
+    int YOB;
+    private int year;
+    static private int startSeed = 1;
+
+    public static void setStartSeed(int startSeed) {PersonGenerator.startSeed = startSeed;}
+    public static int getStartSeed() {return startSeed;}
+
+    public PersonGenerator(String ID, String fName, String lName, String title, int YOB) {
         this.ID = ID;
         this.fName = fName;
         this.lName = lName;
@@ -20,90 +28,55 @@ public class PersonGenerator {
         this.YOB = YOB;
     }
 
-    private String lName;
-    private String title;
-    private int YOB;
-    private int year;
-
-    public String fullName() {
-        return fName + " " +lName;
+    public String getID() {
+        return ID;
     }
-    public String formalName() {
-        return title +""+lName;
+
+    public void setID(String ID) {
+        this.ID = ID;
     }
-    public int getAge() {
-        return YOB - c.get(Calendar.YEAR);
+
+    public String getfName() {
+        return fName;
     }
-    public String getAge(int year) {
-        return null;
+
+    public void setfName(String fName) {
+        this.fName = fName;
     }
-    //int YOB = SafeInput.getRangedInt(in, "Please enter birth year", 1940, 2000);
-    String fileName = "";
-    boolean more = true;
-    Scanner fileNamein = new Scanner(System.in);
 
-    ArrayList<String> People = new ArrayList<>();
-
-    // User enter Persons information
-        do {
-        Scanner in = new Scanner(System.in);
-        String person = "";
-
-
-        //System.out.printf("%10s", person);
-
-        ID = SafeInput.getNonZeroLenString(in, "Please enter ID Number") + ", ";
-        person += ID;
-
-        fName = SafeInput.getNonZeroLenString(in, "Please enter First Name") + ", ";
-        person += fName;
-
-        lName = SafeInput.getNonZeroLenString(in, "Please enter Last Name") + ", ";
-        person += lName;
-
-
-        title = SafeInput.getNonZeroLenString(in, "Please enter persons Title (Mr., Mrs., Ms., Dr., etc.)") + ", ";
-        person += title;
-
-        int Birthyear = SafeInput.getInt(in, "Please enter persons Birth Year");
-        person += Birthyear;
-
-        People.add(person);
-
-        System.out.println("\n");
-        System.out.println(People);
-
-        more = SafeInput.getYNConfirm(in, "Would you like to enter another Person");
-    }while(more);
-
-
-
-
-
-
-    File workingDirectory = new File(System.getProperty("user.dir"));
-    fileName = SafeInput.getNonZeroLenString(fileNamein, "Please enter file name (Don't include .txt)");
-    Path file = Paths.get(workingDirectory.getPath() + "//src//"+fileName+".txt");
-
-        try
-    {
-
-        OutputStream out =
-                new BufferedOutputStream(Files.newOutputStream(file, CREATE));
-        BufferedWriter writer =
-                new BufferedWriter(new OutputStreamWriter(out));
-
-
-        for(String person : People )
-        {
-            writer.write(person, 0, person.length());
-            writer.newLine();
-        }
-        writer.close();
-        System.out.println("Data file written:" + fileName + ".txt");
+    public String getlName() {
+        return lName;
     }
-        catch (IOException e)
-    {
-        e.printStackTrace();
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYOB() {
+        return YOB;
+    }
+
+    public void setYOB(int YOB) {
+        this.YOB = YOB;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonGenerator{" +
+                "ID='" + ID + '\'' +
+                ", fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", title='" + title + '\'' +
+                ", YOB=" + YOB +
+                ", year=" + year +
+                '}';
     }
 }
